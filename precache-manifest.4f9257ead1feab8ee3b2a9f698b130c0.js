6c9c2b0cec38ff2e99fa0a0a778354e736fa08a7
Son Nguyen <qson.ng@gmail.com>
2020-09-24 13:47:42 +0700

    Updates

---
+++ precache-manifest.4f9257ead1feab8ee3b2a9f698b130c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a832cd2775e1a0796d49246cdc19013",
+    "url": "/Useful-Expressions-in-English/index.html"
+  },
+  {
+    "revision": "4f02554d69ca92e8dca1",
+    "url": "/Useful-Expressions-in-English/static/css/main.b10d6e08.chunk.css"
+  },
+  {
+    "revision": "23c73dead787c8b9289f",
+    "url": "/Useful-Expressions-in-English/static/js/2.7404b2d7.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/Useful-Expressions-in-English/static/js/2.7404b2d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f02554d69ca92e8dca1",
+    "url": "/Useful-Expressions-in-English/static/js/main.bde7add9.chunk.js"
+  },
+  {
+    "revision": "dcef8c8079846b93b4ee",
+    "url": "/Useful-Expressions-in-English/static/js/runtime-main.8c03c7a1.js"
+  }
+]);
